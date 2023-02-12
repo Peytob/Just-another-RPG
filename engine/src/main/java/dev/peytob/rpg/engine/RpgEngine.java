03d2f634957878227ec4680301ecadf69da0480b
Roman Reutov <peytob23@gmail.com>
2023-02-12 12:31:04 +0300

    Client module Created client module. RpgEngine is no longer abstract.

--- engine/src/main/java/dev/peytob/rpg/engine/RpgEngine.java
+++ engine/src/main/java/dev/peytob/rpg/engine/RpgEngine.java
@@ -3,8 +3,10 @@ package dev.peytob.rpg.engine;
 import dev.peytob.rpg.engine.pipeline.InitializingPipeline;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
-public abstract class RpgEngine {
+@Component
+public class RpgEngine {
 
     private final Logger logger = LoggerFactory.getLogger(RpgEngine.class);
 
@@ -17,5 +19,6 @@ public abstract class RpgEngine {
     public final void initialize() {
         logger.info("Starting engine initializing pipeline");
         initializingPipeline.execute();
+        logger.info("Engine initializing pipeline completed");
     }
 }
