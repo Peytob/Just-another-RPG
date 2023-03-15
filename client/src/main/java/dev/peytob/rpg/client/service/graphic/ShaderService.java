bfd527dc1c8ccb5ecc7ad693c1a0d5a196b60ca9
Roman Reutov <peytob23@gmail.com>
2023-03-15 20:02:59 +0300

    Shader services

---
+++ client/src/main/java/dev/peytob/rpg/client/service/graphic/ShaderService.java
@@ -0,0 +1,15 @@
+package dev.peytob.rpg.client.service.graphic;
+
+import dev.peytob.rpg.client.resource.Shader;
+import dev.peytob.rpg.client.resource.ShaderProgram;
+
+public interface ShaderService {
+
+    Shader compileShader(String code, String textId, int type);
+
+    ShaderProgram createShaderProgram(Shader vertexShader, Shader fragmentShader, String textId);
+
+    boolean removeShader(Shader shader);
+
+    boolean removeShaderProgram(ShaderProgram shaderProgram);
+}
