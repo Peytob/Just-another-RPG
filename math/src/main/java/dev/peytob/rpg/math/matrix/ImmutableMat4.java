73b0012220488a87e223dd224195444dc443b050
Roman Reutov <peytob23@gmail.com>
2023-04-02 02:33:26 +0600

    Mat4 Added 4x4 matrix

---
+++ math/src/main/java/dev/peytob/rpg/math/matrix/ImmutableMat4.java
@@ -0,0 +1,20 @@
+package dev.peytob.rpg.math.matrix;
+
+final class ImmutableMat4 implements Mat4 {
+
+    private final Mat4 mat4;
+
+    public ImmutableMat4(Mat4 mat4) {
+        this.mat4 = mat4;
+    }
+
+    @Override
+    public float get(int row, int column) {
+        return mat4.get(row, column);
+    }
+
+    @Override
+    public void set(int row, int column, int data) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ math/src/main/java/dev/peytob/rpg/math/matrix/Matrices.java
@@ -0,0 +1,28 @@
+package dev.peytob.rpg.math.matrix;
+
+public enum Matrices {;
+
+    public static Mat4 mutableMat4() {
+        return new MutableMat4();
+    }
+
+    public static Mat4 mutableMat4(float a11, float a12, float a13, float a14,
+                                   float a21, float a22, float a23, float a24,
+                                   float a31, float a32, float a33, float a34,
+                                   float a41, float a42, float a43, float a44) {
+        return new MutableMat4(
+                a11, a12, a13, a14,
+                a21, a22, a23, a24,
+                a31, a32, a33, a34,
+                a41, a42, a43, a44
+        );
+    }
+
+    public static Mat4 immutableMat4(Mat4 mat4) {
+        if (mat4 instanceof ImmutableMat4 immutableMat4) {
+            return immutableMat4;
+        }
+
+        return new ImmutableMat4(mat4);
+    }
+}
