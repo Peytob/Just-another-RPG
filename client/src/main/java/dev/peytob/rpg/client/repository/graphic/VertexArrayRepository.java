f0ba08393f9f9de94adf0f8308d2328aaddb00bc
Roman Reutov <peytob23@gmail.com>
2023-05-06 22:56:33 +0300

    Client and core using new base repositories

---
+++ client/src/main/java/dev/peytob/rpg/client/repository/graphic/VertexArrayRepository.java
@@ -0,0 +1,9 @@
+package dev.peytob.rpg.client.repository.graphic;
+
+import dev.peytob.rpg.client.resource.VertexArray;
+import dev.peytob.rpg.engine.repositry.BaseRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VertexArrayRepository extends BaseRepository<VertexArray> {
+}
