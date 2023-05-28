6e81d073f0c1043900cca17272795b948faf7e2e
Roman Reutov <peytob23@gmail.com>
2023-05-28 22:47:16 +0300

    Default uniform blocks Added default uniform blocks creating.

--- client/src/main/java/dev/peytob/rpg/client/module/graphic/repository/UniformBlockRepository.java
+++ client/src/main/java/dev/peytob/rpg/client/module/graphic/repository/UniformBlockRepository.java
@@ -1,9 +1,9 @@
 package dev.peytob.rpg.client.module.graphic.repository;
 
-import dev.peytob.rpg.client.module.graphic.resource.UniformBuffer;
+import dev.peytob.rpg.client.module.graphic.resource.UniformBlock;
 import dev.peytob.rpg.engine.repositry.BaseRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UniformBufferRepository extends BaseRepository<UniformBuffer> {
+public class UniformBlockRepository extends BaseRepository<UniformBlock> {
 }
--- math/src/main/java/dev/peytob/rpg/math/matrix/Mat4.java
+++ math/src/main/java/dev/peytob/rpg/math/matrix/Mat4.java
@@ -2,6 +2,8 @@ package dev.peytob.rpg.math.matrix;
 
 public interface Mat4 {
 
+    int BYTES = Float.BYTES * 4 * 4;
+
     float get(int row, int column);
 
     void set(int row, int column, int data);
