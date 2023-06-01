d3757851a53aa45fd350dca317278eb531c86afd
Roman Reutov <peytob23@gmail.com>
2023-06-01 22:50:19 +0300

    Texture resource Added texture resource. Added service for creating and deleting textures.

---
+++ client/src/main/java/dev/peytob/rpg/client/module/graphic/resource/Texture.java
@@ -0,0 +1,16 @@
+package dev.peytob.rpg.client.module.graphic.resource;
+
+import dev.peytob.rpg.engine.resource.Resource;
+import dev.peytob.rpg.math.vector.Vec2i;
+
+import java.nio.ByteBuffer;
+
+public record Texture(
+    Integer id,
+    String textId,
+    Integer format,
+    Integer dataType,
+    Vec2i sizes,
+    ByteBuffer data
+) implements Resource {
+}
